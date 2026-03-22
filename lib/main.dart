eb51792fb6bf88486776d0cfe56eb2bc515c692c
panida sotthpok <s665210010@sau.ac.th>
2026-03-22 14:03:34 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sau_life2/views/splash_screen_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Fluttersaulife());
@@ -14,6 +16,12 @@ class Fluttersaulife extends StatefulWidget {
 class _FluttersaulifeState extends State<Fluttersaulife> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreenUi(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.kanitTextTheme(Theme.of(context).textTheme,)
+      ),
+    );
   }
 }
