40a6d7a0a28955c8f4e71039e6bbd62f61b2fd43
panida sotthpok <s665210010@sau.ac.th>
2026-03-22 11:56:41 +0700

    update

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sau_life2/views/About_ui.dart';
+import 'package:flutter_sau_life2/views/bmi_ui.dart';
+import 'package:flutter_sau_life2/views/bmr_ui.dart';
 
 class HomeUi extends StatefulWidget {
   const HomeUi({super.key});
@@ -8,8 +11,41 @@ class HomeUi extends StatefulWidget {
 }
 
 class _HomeUiState extends State<HomeUi> {
+  int selectedIndex = 1;
+  List subPage = [
+    BmiUi(), 
+    AboutUi(), 
+    BmrUi()
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      // Appbar
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text('BHC App', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.deepOrange,
+        unselectedItemColor: Colors.grey[400],
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'BMI'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'เกี่ยวกับ'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_run_outlined),
+            label: 'BMR',
+          ),
+        ],
+      ),
+      body: subPage[0],
+    );
   }
 }
